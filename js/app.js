72ee80d01eedbe89d08cbbf34187c6e9b182bcbc
souro18 <sourajit.paul61@gmail.com>
2018-08-26 14:21:37 +0530

    bug and player move added

--- js/app.js
+++ js/app.js
@@ -1,36 +1,104 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+// var Enemy = function() {
+//     // Variables applied to each of our instances go here,
+//     // we've provided one for you to get started
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+//     // The image/sprite for our enemies, this uses
+//     // a helper we've provided to easily load images
+//     this.sprite = 'images/enemy-bug.png';
+// };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+// // Update the enemy's position, required method for game
+// // Parameter: dt, a time delta between ticks
+// Enemy.prototype.update = function(dt) {
+//     // You should multiply any movement by the dt parameter
+//     // which will ensure the game runs at the same speed for
+//     // all computers.
+// };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// // Draw the enemy on the screen, required method for game
+// Enemy.prototype.render = function() {
+//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// };
+ 
 
+class Enemy {
+    constructor(y=0,speed=20,sprite = 'images/enemy-bug.png'){
+        this.sprite=sprite;
+        this.x=0;
+        this.y= 90*y+50;
+        this.speed=speed;
+    }
+    update(dt){
+        this.x+=(this.speed*dt);
+        if(this.x>=400){
+            this.x=0;
+            console.log('update');
+        }
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor(sprite='images/char-boy.png' , x=200,y= 320){
+        this.sprite=sprite;
+        this.x=x;
+        this.y=y;
+    }
+    update(){
 
+    }
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(move){
+        if(move==='left'){
+          //  console.log('left');
+            if(this.x>0){
+        //        console.log('leftmove');
+                this.x-=100;
+            }
+        }
+        if(move==='right'){
+            //console.log('right');
+            if(this.x<400){
+                this.x+=100;
+             //   console.log('rightmove');
+            }
+        }
+        if(move==='up'){
+            //console.log('up');
+            if(this.y>0){
+                this.y-=90;
+             //   console.log('upmove');
+             if(this.y<0){
+                this.y=320;
+             }
+            }
+        }
+        if(move==='down'){
+         //  console.log('down');
+            if(this.y<400){
+                this.y+=90;
+           //     console.log('downmove');
+            }
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+const e1= new Enemy(0,50);
+const e2= new Enemy(1,30);
+const e3= new Enemy(0,10);
+const e4= new Enemy(2,40);
+const allEnemies=[e1,e2,e3,e4];
+const player=new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
